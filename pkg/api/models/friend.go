99a5ede9110baa83923d9a9942c2492a397b0ee2
jaxron <126792083+jaxron@users.noreply.github.com>
2024-09-30 13:32:40 +0800

    fix(friends): correct invalid types

--- pkg/api/models/friend.go
+++ pkg/api/models/friend.go
@@ -14,14 +14,14 @@ type UserResponse struct {
 	ID                     uint64    `json:"id"`                     // Unique identifier for the friend
 	Name                   string    `json:"name"`                   // Username of the friend
 	DisplayName            string    `json:"displayName"`            // Display name of the friend
-	Description            string    `json:"description"`            // Friend's profile description
+	Description            *string   `json:"description"`            // Friend's profile description
 	Created                time.Time `json:"created"`                // Date when the friend account was created
 	IsBanned               bool      `json:"isBanned"`               // Whether the friend is banned
 	ExternalAppDisplayName *string   `json:"externalAppDisplayName"` // Display name for external apps (if any)
 	HasVerifiedBadge       bool      `json:"hasVerifiedBadge"`       // Whether the friend has a verified badge
 	FriendFrequentScore    int       `json:"friendFrequentScore"`    // Friend frequent score
 	FriendFrequentRank     int       `json:"friendFrequentRank"`     // Friend frequent rank
-	PresenceType           string    `json:"presenceType"`           // Type of presence
+	PresenceType           int       `json:"presenceType,omitempty"` // Type of presence
 	IsOnline               bool      `json:"isOnline"`               // Whether the friend is online
 	IsDeleted              bool      `json:"isDeleted"`              // Whether the friend account is deleted
 }
