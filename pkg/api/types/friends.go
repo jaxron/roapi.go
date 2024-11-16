ee0b57b6c5573a557ffce5b93df87c11812ebfa9
jaxron <126792083+jaxron@users.noreply.github.com>
2024-11-16 10:24:17 +0800

    fix(types): friends endpoint changes

--- pkg/api/types/friends.go
+++ pkg/api/types/friends.go
@@ -1,7 +1,5 @@
 package types
 
-import "time"
-
 // PresenceType represents the type of user presence.
 type PresenceType string
 
@@ -34,31 +32,7 @@ type FollowingPageResponse struct {
 
 // Friend represents a single friend in a user's friend list.
 type Friend struct {
-	ID                     uint64    `json:"id"`                     // Unique identifier for the friend
-	Name                   string    `json:"name"`                   // Username of the friend
-	DisplayName            string    `json:"displayName"`            // Display name of the friend
-	Description            *string   `json:"description"`            // Friend's profile description
-	Created                time.Time `json:"created"`                // Date when the friend account was created
-	IsBanned               bool      `json:"isBanned"`               // Whether the friend is banned
-	ExternalAppDisplayName *string   `json:"externalAppDisplayName"` // Display name for external apps (if any)
-	HasVerifiedBadge       bool      `json:"hasVerifiedBadge"`       // Whether the friend has a verified badge
-	FriendFrequentScore    int       `json:"friendFrequentScore"`    // Friend frequent score
-	FriendFrequentRank     int       `json:"friendFrequentRank"`     // Friend frequent rank
-	PresenceType           int       `json:"presenceType,omitempty"` // Type of presence
-	IsOnline               bool      `json:"isOnline"`               // Whether the friend is online
-	IsDeleted              bool      `json:"isDeleted"`              // Whether the friend account is deleted
-}
-
-// UserPresenceResponse represents the structure of friend information returned by the Roblox API.
-type UserPresenceResponse struct {
-	UserPresenceType PresenceType `json:"userPresenceType"` // Type of user presence
-	UserLocationType LocationType `json:"userLocationType"` // Type of user location
-	LastLocation     string       `json:"lastLocation"`     // Last location of the user
-	PlaceID          *uint64      `json:"placeId"`          // Place ID the user is in
-	RootPlaceID      *uint64      `json:"rootPlaceId"`      // Root place ID the user is in
-	GameInstanceID   *string      `json:"gameInstanceId"`   // Game instance ID the user is in
-	UniverseID       *uint64      `json:"universeId"`       // Universe ID the user is in
-	LastOnline       time.Time    `json:"lastOnline"`       // Last online time of the user
+	ID uint64 `json:"id"` // Unique identifier for the friend
 }
 
 // FriendPageResponse represents the structure of a user's friend list returned by the Roblox API.
@@ -78,7 +52,5 @@ type FriendResponse struct {
 // OnlineFriend represents the structure of friend information returned by the Roblox API.
 type OnlineFriend struct {
 	ID           uint64               `json:"id"`           // Unique identifier for the friend
-	Name         string               `json:"name"`         // Username of the friend
-	DisplayName  string               `json:"displayName"`  // Display name of the friend
 	UserPresence UserPresenceResponse `json:"userPresence"` // User presence information
 }
